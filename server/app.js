f020464380b7159169e328d6b885b91b076d0535
River Jensen <river@code1088.com>
2024-03-24 17:43:28 -0600

    server

---
+++ server/app.js
@@ -0,0 +1,23 @@
+import  express  from "express";
+import morgan from "morgan";
+import ViteExpress from "vite-express"
+import  session  from "express-session";
+
+const app = express()
+const port = "1088"
+
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
+app.use(express.json());
+app.use(
+  session({
+    secret: "ssshhhhh",
+    saveUninitialized: true,
+    resave: false,
+  })
+);
+
+ViteExpress.listen(app, port, () =>
+  console.log(`Server is listening on http://localhost:${port}`)
+);
