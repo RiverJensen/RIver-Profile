4e45d933baaca0bd21d98f243b947e25236300d1
River Jensen <river@code1088.com>
2024-04-03 15:20:02 -0700

    added the server

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,36 @@
-import { useState } from 'react'
-import axios from 'axios';
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import './App.css'
+import { useState } from "react";
+import axios from "axios";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  BrowserRouter,
+  Routes,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+import "./App.css";
+import "./css/api-image.css";
+import HomePage from "./components/HomePage";
+import ApiRequest from "./components/ApiRequest";
+import ProjectPage from "./components/ProjectPage";
+import AboutPage from "./components/AboutPage";
+import Root from "./Root";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Root />}>
+      <Route index element={<HomePage />} />
+      <Route path="/Project" element={<ProjectPage />} />
+      <Route path="/About" element={<AboutPage />} />
+    </Route>
+  )
+);
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  const [imageUrl, setImageUrl] = useState(null);
-
-  const fetchImage = async () => {
-    try {
-      const response = await axios.get('https://api.thecatapi.com/v1/images/search?order=random&format=gif');
-      const imageUrl = response.data[0].url;
-      setImageUrl(imageUrl);
-    } catch (error) {
-      console.error('Error fetching image:', error);
-    }
-  };
-
-  return (
-    <>
-      <div>
-        <h3>Test</h3>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <div>
-      <button onClick={fetchImage}>Fetch Image</button>
-      {imageUrl && <img src={imageUrl} alt="Cat" />}
-    </div>
-    </>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
